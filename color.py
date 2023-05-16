543027034ffab316621219c6c28682a772b41c08
Fourcaudot Tom <tom.fourcaudot@gmail.com>
2023-05-16 22:26:36 +0900

    Add colors constants

---
+++ color.py
@@ -0,0 +1,14 @@
+import pygame
+
+black =  pygame.Color(0, 0, 0, 255)
+blue =  pygame.Color(0, 0, 255, 255)
+cyan =  pygame.Color(0, 255, 255, 255)
+gold =  pygame.Color(255, 215, 0, 255)
+gray =  pygame.Color(190, 190, 190, 255)
+green =  pygame.Color(0, 255, 0, 255)
+orange =  pygame.Color(255, 165, 0, 255)
+purple =  pygame.Color(160, 32, 240, 255)
+red =  pygame.Color(255, 0, 0, 255)
+violet =  pygame.Color(238, 130, 238, 255)
+yellow =  pygame.Color(255, 255, 0, 255)
+white =  pygame.Color(255, 255, 255, 255)
